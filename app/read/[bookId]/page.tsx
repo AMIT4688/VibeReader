c1876c6ceb80833576d986aa96c8eb1e65940121
AMIT4688 <84274552+AMIT4688@users.noreply.github.com>
2025-11-10 22:57:26 +0530

    Updated page.tsx

--- app/read/[bookId]/page.tsx
+++ app/read/[bookId]/page.tsx
@@ -1,5 +1,12 @@
 import ReadBookClient from './ReadBookClient';
 
+export async function generateStaticParams() {
+  return [];
+}
+
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+
 export default function ReadBookPage() {
   return <ReadBookClient />;
 }
